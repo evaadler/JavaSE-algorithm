6b2c67014ba41e33e6148395c3bb46db9c218048
alicia <jiandanfifi>
2020-06-11 08:49:28 +0800

    使用异常而非返回码（采用异常处理）

---
+++ src/main/java/cleancode/old/exception/DeviceID.java
@@ -0,0 +1,11 @@
+package cleancode.old.exception;
+
+/**
+ * DeviceID
+ *
+ * @author Alicia
+ * @description
+ * @date 2020/6/11
+ */
+public class DeviceID {
+}
---
+++ src/main/java/cleancode/old/exception/DeviceRecord.java
@@ -0,0 +1,11 @@
+package cleancode.old.exception;
+
+/**
+ * DeviceRecord
+ *
+ * @author Alicia
+ * @description
+ * @date 2020/6/11
+ */
+public class DeviceRecord {
+}
