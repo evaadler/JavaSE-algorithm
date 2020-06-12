a128a17774cc62c54199bf9c01295b0e38f3fd43
alicia <jiandanfifi>
2020-06-12 09:28:38 +0800

    违反SPR原则

---
+++ src/main/java/cleancode/old/sql/Column.java
@@ -0,0 +1,11 @@
+package cleancode.old.sql;
+
+/**
+ * Column
+ *
+ * @author Alicia
+ * @description
+ * @date 2020/6/12
+ */
+public class Column {
+}
---
+++ src/main/java/cleancode/old/sql/Criteria.java
@@ -0,0 +1,11 @@
+package cleancode.old.sql;
+
+/**
+ * Criteria
+ *
+ * @author Alicia
+ * @description
+ * @date 2020/6/12
+ */
+public class Criteria {
+}
