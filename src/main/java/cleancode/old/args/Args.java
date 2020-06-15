618f5827edc5e890f6d4c11260933b8489112c06
alicia <jiandanfifi>
2020-06-15 22:50:54 +0800

    添加String类型参数（整个混乱了）

--- src/main/java/cleancode/old/args/Args.java
+++ src/main/java/cleancode/old/args/Args.java
@@ -1,11 +1,9 @@
 package cleancode.old.args;
 
-import jdk.nashorn.internal.ir.IfNode;
+import sun.util.locale.ParseStatus;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.text.ParseException;
+import java.util.*;
 
 /**
  * Args
@@ -18,51 +16,84 @@ public class Args {
     private String schema;
     private String[] args;
     private boolean valid;
-    private Set<Character> unexpectArguments = new TreeSet<Character>();
+    private Set<Character> unexpectedArguments = new TreeSet<Character>();
     private Map<Character, Boolean> booleanArgs = new HashMap<Character, Boolean>();
-    private int numberOfArguments = 0;
 
-    public Args(String schema, String[] args) {
+    private Map<Character, String> stringArgs = new HashMap<Character, String>();
+    private Set<Character> argsFound = new HashSet<>();
+    private int currentArgument;
+    private char errorArgument = '\0';
+
+    enum ErrorCode {
+        OK, MISSING_STRING
+    }
+
+    private ErrorCode errorCode = ErrorCode.OK;
+
+    public Args(String schema, String[] args) throws ParseException{
         this.schema = schema;
         this.args = args;
         valid = parse();
     }
 
-    public boolean isValid() {
-        return valid;
-    }
 
-    private boolean parse() {
+    private boolean parse() throws ParseException{
         if (schema.length() == 0 && args.length == 0) {
             return true;
         }
         parseSchema();
         parseArguments();
-        return unexpectArguments.size() == 0;
+        return valid;
     }
 
-    private boolean parseSchema() {
+    private boolean parseSchema() throws ParseException{
         for (String element : schema.split(",")) {
-            parseSchemaElement(element);
+            if (element.length() > 0) {
+                String trimmedElement = element.trim();
+                parseSchemaElement(trimmedElement);
+            }
         }
         return true;
     }
 
-    private void parseSchemaElement(String element) {
-        if (element.length() == 1) {
-            parseBooleanSchemaElement(element);
+    private void parseSchemaElement(String element) throws ParseException{
+        char elementId = element.charAt(0);
+        String elementTail = element.substring(1);
+        validateSchemaElementId(elementId);
+        // TODO boi 加商品类型那块可以参考这里
+        if (isBooleanSchemaElement(elementTail)){
+            parseBooleanSchemaElement(elementId);
+        }else if (isStringSchemaElement(elementTail)){
+            parseStringSchemaElement(elementId);
         }
     }
 
-    private void parseBooleanSchemaElement(String element) {
-        char c = element.charAt(0);
-        if (Character.isLetter(c)) {
-            booleanArgs.put(c, false);
+    private void validateSchemaElementId(char elementId) throws ParseException {
+        if (!Character.isLetter(elementId)) {
+            // boi中抛异常抽取那块可以参考这里
+            throw new ParseException("Bad character:" + elementId + "in Args format " + schema, 0);
         }
+    }
+
+    private void parseStringSchemaElement(char elementId) {
+        stringArgs.put(elementId, "");
+    }
+
+    private boolean isStringSchemaElement(String elementTail) {
+        return elementTail.equals("*");
+    }
+
+    private boolean isBooleanSchemaElement(String elementTail) {
+        return elementTail.length() == 0;
+    }
+
+    private void parseBooleanSchemaElement(char elementId) {
+        booleanArgs.put(elementId, false);
     }
 
     private boolean parseArguments() {
-        for (String arg: args) {
+        for (currentArgument = 0; currentArgument < args.length; currentArgument++) {
+            String arg = args[currentArgument];
             parseArgument(arg);
         }
         return true;
@@ -81,13 +112,43 @@ public class Args {
     }
 
     private void parseElement(char argChar) {
-        if (isBoolean(argChar)) {
-            numberOfArguments ++;
-            setBooleanArg(argChar, true);
+        if (setArgument(argChar)) {
+            argsFound.add(argChar);
         } else {
-            unexpectArguments.add(argChar);
+            unexpectedArguments.add(argChar);
+            valid = false;
         }
     }
+
+    private boolean setArgument(char argChar) {
+        boolean set = true;
+        if (isBoolean(argChar)){
+            setBooleanArg(argChar, true);
+        }else if(isString(argChar)){
+            setStringArg(argChar, "");
+        }else{
+            set = false;
+        }
+        return set;
+    }
+
+    private void setStringArg(char argChar, String s) {
+        currentArgument ++;
+        try {
+            stringArgs.put(argChar, args[currentArgument]);
+        }catch (ArrayIndexOutOfBoundsException e) {
+            valid = false;
+            errorArgument = argChar;
+            errorCode = ErrorCode.MISSING_STRING;
+        }
+
+    }
+
+    private boolean isString(char argChar) {
+        return stringArgs.containsKey(argChar);
+    }
+
+
 
     private void setBooleanArg(char argChar, boolean value) {
         booleanArgs.put(argChar, value);
@@ -98,7 +159,7 @@ public class Args {
     }
 
     public int cardinality() {
-        return numberOfArguments;
+        return argsFound.size();
     }
 
     public String usage() {
@@ -109,17 +170,24 @@ public class Args {
         }
     }
 
-    public String errorMessage() {
-        if (unexpectArguments.size() > 0) {
+    public String errorMessage() throws Exception{
+        if (unexpectedArguments.size() > 0) {
             return unexpectArgumentMessage();
         } else {
+            switch (errorCode) {
+                case MISSING_STRING:
+                    return String.format("Could not find string parameter for -%c.",
+                            errorArgument);
+                case OK:
+                    throw new Exception("TILT: Should not get here.");
+            }
             return "";
         }
     }
 
     private String unexpectArgumentMessage() {
         StringBuffer message = new StringBuffer("Argument(s) -");
-        for (char c : unexpectArguments) {
+        for (char c : unexpectedArguments) {
             message.append(c);
         }
         message.append(" unexpected");
@@ -128,6 +196,26 @@ public class Args {
 
     public boolean getBoolean(char arg) {
         return booleanArgs.get(arg);
+    }
+
+    private boolean falseIfNull(Boolean b) {
+        return b == null ? false : b;
+    }
+
+    public String getString(char arg){
+        return blankIfNull(stringArgs.get(arg));
+    }
+
+    private String blankIfNull(String s) {
+        return s==null ? "":s;
+    }
+
+    public boolean has(char arg){
+        return argsFound.contains(arg);
+    }
+
+    public boolean isValid() {
+        return valid;
     }
 
 }
