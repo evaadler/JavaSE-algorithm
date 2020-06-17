48bb8b50570e607cf2add28181f4b160060b81b5
alicia <jiandanfifi>
2020-06-17 22:44:10 +0800

    模板模式-回调方式

---
+++ src/main/java/cleancode/modelMethod/BClass.java
@@ -0,0 +1,14 @@
+package cleancode.modelMethod;
+
+/**
+ * BClass
+ *
+ * @author Alicia
+ * @description
+ * @date 2020/6/17
+ */
+public class BClass {
+    public void process(ICallback callback){
+        callback.methodToCallback();
+    }
+}
---
+++ src/main/java/cleancode/modelMethod/ICallback.java
@@ -0,0 +1,12 @@
+package cleancode.modelMethod;
+
+/**
+ * ICallback
+ *
+ * @author Alicia
+ * @description
+ * @date 2020/6/17
+ */
+public interface ICallback {
+    void methodToCallback();
+}
