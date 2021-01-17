49fffaa7df3d7953e82160f963ada7e0eb8bc7dd
alicia <jiandanfifi>
2021-01-17 11:58:01 +0800

    工厂模式 -- 工厂方法

---
+++ src/main/java/designpattern/factory/factorymethod/pizza/BJPepperPizza.java
@@ -0,0 +1,16 @@
+package designpattern.factory.factorymethod.pizza;
+
+/**
+ * PepperPizza
+ *
+ * @author Alicia
+ * @description
+ * @date 2021/1/17
+ */
+public class BJPepperPizza extends Pizza {
+    @Override
+    public void prepare() {
+        setName("北京的奶酪");
+        System.out.println("给北京胡椒披萨准备原材料");
+    }
+}
---
+++ src/main/java/designpattern/factory/factorymethod/pizza/LDPepperPizza.java
@@ -0,0 +1,16 @@
+package designpattern.factory.factorymethod.pizza;
+
+/**
+ * PepperPizza
+ *
+ * @author Alicia
+ * @description
+ * @date 2021/1/17
+ */
+public class LDPepperPizza extends Pizza {
+    @Override
+    public void prepare() {
+        setName("伦敦的胡椒");
+        System.out.println("给北京胡椒披萨准备原材料");
+    }
+}
