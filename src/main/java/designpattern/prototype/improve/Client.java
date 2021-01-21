78cd8ae5282030eb376062ad90345f7c80abf490
alicia <jiandanfifi>
2021-01-21 11:05:00 +0800

    s

--- src/main/java/designpattern/prototype/improve/Client.java
+++ src/main/java/designpattern/prototype/improve/Client.java
@@ -10,7 +10,7 @@ package designpattern.prototype.improve;
 public class Client {
     public static void main(String[] args) {
         System.out.println("原型模式完成对象的创建");
-        Sheep sheep = new Sheep("tom", 1);
+        Sheep sheep = new Sheep("tom", 1, "dd");
         Sheep sheep1 = (Sheep)sheep.clone(); // 克隆
         System.out.println(sheep);
         System.out.println(sheep1);
