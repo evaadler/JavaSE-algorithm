ae43a7088c7cb24da86669199fe02a38652b2fb0
alicia <jiandanfifi>
2021-08-08 23:09:20 +0800

    接口隔离原则

---
+++ src/main/java/designpattern/interfaceisolation/positive/Swimable.java
@@ -0,0 +1,12 @@
+package designpattern.interfaceisolation.positive;
+
+/**
+ * Flyable
+ *
+ * @author Alicia
+ * @description
+ * @date 2021/8/8
+ */
+public interface Swimable {
+    void swim();
+}
