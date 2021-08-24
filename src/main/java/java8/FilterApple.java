f1a5f5657132a31d781364a891230afd88840aba
alicia <jiandanfifi>
2021-08-24 22:52:24 +0800

    lambda 浅尝辄止

--- src/main/java/java8/FilterApple.java
+++ src/main/java/java8/FilterApple.java
@@ -1,6 +1,8 @@
 package java8;
 
 
+import sun.java2d.loops.GraphicsPrimitive;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,10 +15,16 @@ import java.util.List;
  * @date 2021/8/22
  */
 public class FilterApple {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         List<Apple> list = Arrays.asList(new Apple("green", 150), new Apple("yellow", 120), new Apple("green", 170));
         List<Apple> result = findApple(list, new GreenAnd150WeightFilter());
         System.out.println(result);
+
+        List<Apple> lambdaResult = findApple(list, (apple) -> {
+            return apple.getColor().equals("green");
+        });
+
+        System.out.println(lambdaResult);
 
         List<Apple> yellowApples = findApple(list, new AppleFilter() {
             @Override
@@ -25,8 +33,23 @@ public class FilterApple {
             }
         });
         System.out.println(yellowApples);
+
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        });
+        thread.start();
+
+        //Thread.currentThread().join();
+
+        new Thread(() -> System.out.println(Thread.currentThread().getName())).start();
+
     }
 
+    @FunctionalInterface//只有一个方法的时候可以写这个注解
     public interface AppleFilter{
         /**
          * 过滤条件
