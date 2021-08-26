ff4971920224cf5fa61adfaec36d7236f238f806
alicia <jiandanfifi>
2021-08-26 09:09:58 +0800

    lambda 常见用法

---
+++ src/main/java/java8/LambdaExpression.java
@@ -0,0 +1,50 @@
+package java8;
+
+
+import ningM.com.adobe.app.App;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+/**
+ * LambdaExpression
+ *
+ * @author Alicia
+ * @description
+ * @date 2021/8/24
+ */
+public class LambdaExpression {
+
+    public static void main(String[] args) {
+        Comparator<Apple> byColor = new Comparator<Apple>() {
+            @Override
+            public int compare(Apple o1, Apple o2) {
+                return o1.getColor().compareTo(o2.getColor());
+            }
+        };
+
+        List<Apple> list = Collections.emptyList();
+        list.sort(byColor);
+        // 这块没有 return
+        Comparator<Apple> byColorLambda = (o1, o2) -> o1.getColor().compareTo(o2.getColor());
+        // 如果加了{} 花括号就要 return 了
+        Comparator<Apple> byColorLambda2 = (o1, o2) -> {return o1.getColor().compareTo(o2.getColor());};
+
+        Function<String, Integer> stringConsumer = s -> s.length();
+
+        // 按以前的理解，就是已经有了一个实现类了，后面就可以用了
+        Predicate<Apple> p = a -> a.getColor().equals("green");
+
+        Runnable r = ()->{};
+
+        //一个入参，一个返回值；BiFunction：两个入参，一个返回值
+        Function<Apple, Boolean> f = (a) -> a.getColor().equals("green");
+
+        //get: 拿到一个想要的东西
+        Supplier<Apple> s = Apple::new;
+    }
+}
