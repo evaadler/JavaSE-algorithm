952ed8f0bc7634331bf428edb352914b8ef15739
alicia <jiandanfifi>
2021-09-01 07:52:33 +0800

    ThreeFunction

---
+++ src/main/java/designpattern/state/work/TransactionState.java
@@ -0,0 +1,14 @@
+package designpattern.state.work;
+
+/**
+ * TransactionState
+ *
+ * @author Alicia
+ * @description
+ * @date 2021/8/21
+ */
+public abstract class TransactionState{
+    // overdue
+    // settle
+    // early pay
+}
